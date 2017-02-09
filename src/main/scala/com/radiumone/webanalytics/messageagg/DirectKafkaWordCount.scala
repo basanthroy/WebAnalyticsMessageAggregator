aaf5a30a53dc3e7465a77b0c90f19dec66197003
Basanth Roy <broy@radiumone.com>
2017-02-08 18:58:11 -0800

    Commenting out a few incompatible lines in DirectKafkaWordCount

--- src/main/scala/com/radiumone/webanalytics/messageagg/DirectKafkaWordCount.scala
+++ src/main/scala/com/radiumone/webanalytics/messageagg/DirectKafkaWordCount.scala
@@ -21,6 +21,7 @@ package com.radiumone.webanalytics.messageagg
 import kafka.serializer.StringDecoder
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming._
+import org.apache.spark.streaming.kafka010.KafkaUtils
 
 /**
  * Consumes messages from one or more topics in Kafka and does wordcount.
@@ -48,25 +49,25 @@ object DirectKafkaWordCount {
 
     val Array(brokers, topics) = args
 
-    // Create context with 2 second batch interval
-    val sparkConf = new SparkConf().setAppName("DirectKafkaWordCount")
-    val ssc = new StreamingContext(sparkConf, Seconds(2))
-
-    // Create direct kafka stream with brokers and topics
-    val topicsSet = topics.split(",").toSet
-    val kafkaParams = Map[String, String]("metadata.broker.list" -> brokers)
-    val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
-      ssc, kafkaParams, topicsSet)
-
-    // Get the lines, split them into words, count the words and print
-    val lines = messages.map(_._2)
-    val words = lines.flatMap(_.split(" "))
-    val wordCounts = words.map(x => (x, 1L)).reduceByKey(_ + _)
-    wordCounts.print()
-
-    // Start the computation
-    ssc.start()
-    ssc.awaitTermination()
+//    // Create context with 2 second batch interval
+//    val sparkConf = new SparkConf().setAppName("DirectKafkaWordCount")
+//    val ssc = new StreamingContext(sparkConf, Seconds(2))
+//
+//    // Create direct kafka stream with brokers and topics
+//    val topicsSet = topics.split(",").toSet
+//    val kafkaParams = Map[String, String]("metadata.broker.list" -> brokers)
+//    val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
+//      ssc, kafkaParams, topicsSet)
+//
+//    // Get the lines, split them into words, count the words and print
+//    val lines = messages.map(_._2)
+//    val words = lines.flatMap(_.split(" "))
+//    val wordCounts = words.map(x => (x, 1L)).reduceByKey(_ + _)
+//    wordCounts.print()
+//
+//    // Start the computation
+//    ssc.start()
+//    ssc.awaitTermination()
   }
 }
 // scalastyle:on println
